0e2fe0f0e6bf104fd1d9bc994d7d0603423093fa
matheus2005hidalgo-arch <matheus2005hidalgo@gmail.com>
2025-10-22 21:18:43 -0300

    Add files via upload

---
+++ soma.js
@@ -0,0 +1,8 @@
+alert("soma de dois numeros inteiros")
+let numero01 = Number(prompt("Digite o primeiro numero"))
+let numero02 = Number(prompt("Digite o Segundo numero"))
+
+let resultado = numero01 + numero02
+
+alert(resultado)
+
