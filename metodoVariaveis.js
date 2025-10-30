fa693c90b9a42796da6372aad746c911caa88355
matheus2005hidalgo-arch <matheus2005hidalgo@gmail.com>
2025-10-30 20:39:57 -0300

    Add files via upload

---
+++ metodoVariaveis.js
@@ -0,0 +1,9 @@
+let divisao = 10 / 3
+alert(divisao.tolofixed(2))
+
+
+//alert(dinheiro.tolocaleString(`pt-br`,
+//{style: `currency`, currency: "brl"}))
+//alert(nome.toLocaleLowerCase()) MENUSCULO
+//alert(nome.toUpperCase()) TUDO MAIUSCULO
+//alert(nome.length) QUANTOS LATRAS TEM
